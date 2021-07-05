733d2c0a20069d992af7559d533b63b14dba52b3
Edoardo Pasca <edo.paskino@gmail.com>
2021-07-05 16:56:42 +0100

    bump version to 1.0.2

--- brem/__init__.py
+++ brem/__init__.py
@@ -25,7 +25,7 @@ modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [basename(f)[:-3] for f in modules if isfile(f)
            and not f.endswith('__init__.py')]
 
-__version__ = '1.0.0'
+__version__ = '1.0.2'
 
 try:
     from .brem import BasicRemoteExecutionManager
