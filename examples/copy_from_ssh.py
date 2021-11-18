dd48c7f7d8b92102626eb21fc9f4252785b77170
Edoardo Pasca <edo.paskino@gmail.com>
2021-11-18 16:51:29 +0000

    Async copy over ssh (#41)
    * added class to handle async file copy
    * add test script and AsyncCopyOverSSH
    * added docstring and Windows and POSIX variables

---
+++ examples/copy_from_ssh.py
@@ -0,0 +1,172 @@
+from brem import AsyncCopyOverSSH
+import os
+from PySide2 import QtCore
+from eqt.threading import Worker
+# import pysnooper
+import brem
+from brem.brem import BasicRemoteExecutionManager
+
+from time import sleep
+
+class RemoteAsyncCopyFromSSHSignals(QtCore.QObject):
+    status = QtCore.Signal(tuple)
+    job_id = QtCore.Signal(int)
+class AsyncCopyFromSSH(object):
+    def __init__(self, parent=None):
+
+        self.internalsignals = RemoteAsyncCopyFromSSHSignals()
+        self.threadpool = QtCore.QThreadPool()
+        self.logfile = 'AsyncCopyFromSSH.log'
+        self._worker = None
+        
+    def SetRemoteFileName(self, dirname, filename):
+        self.remotefile = filename
+        self.remotedir = dirname
+    def SetDestinationDir(self, dirname):
+        self.localdir = dirname
+
+    @property
+    def signals(self):
+        return self.worker.signals
+
+    @property
+    def worker(self):
+        if self._worker is None:
+            print("creating worker")
+            username = self.connection_details['username']
+            port = self.connection_details['port']
+            host = self.connection_details['host']
+            private_key = self.connection_details['private_key']
+            localdir = self.localdir
+
+            self._worker = Worker(self.copy_worker, 
+                                  remotedir=self.remotedir, 
+                                  remotefile = self.remotefile,
+                                  host=host, 
+                                  username=username, 
+                                  port=port, 
+                                  private_key=private_key, 
+                                  logfile=self.logfile, 
+                                  update_delay=10, 
+                                  localdir=localdir)
+        return self._worker
+
+    def setRemoteConnectionSettings(self, username=None, 
+                                    port= None, host=None, private_key=None, localdir=None):
+        self.connection_details = {'username': username, 
+                                   'port': port,
+                                   'host': host, 
+                                   'private_key': private_key,
+                                   'localdir': localdir}
+    def copy_worker(self, **kwargs):
+        # retrieve the appropriate parameters from the kwargs
+        host         = kwargs.get('host', None)
+        username     = kwargs.get('username', None)
+        port         = kwargs.get('port', None)
+        private_key  = kwargs.get('private_key', None)
+        logfile      = kwargs.get('logfile', None)
+        update_delay = kwargs.get('update_delay', None)
+        remotefile   = kwargs.get('remotefile', None)
+        remotedir    = kwargs.get('remotedir', None)
+        localdir     = kwargs.get('localdir', None)
+        # get the callbacks
+        message_callback  = kwargs.get('message_callback', None)
+        progress_callback = kwargs.get('progress_callback', None)
+        status_callback   = kwargs.get('status_callback', None)
+        for k,v in kwargs.items():
+            print (k,v)
+
+        if remotefile is not None:
+
+            
+            
+            
+            a=BasicRemoteExecutionManager(host=host,username=username,port=22,private_key=private_key)
+
+            a.login(passphrase=False)
+            
+            # if message_callback is not None:
+            #     message_callback.emit("{}".format(tail.decode('utf-8')))
+            # set the progress to 100
+            if progress_callback is not None:
+                progress_callback.emit(0)
+
+            a.changedir(remotedir)
+            cwd = os.getcwd()
+            os.chdir(localdir)
+            print("get file", remotefile)
+            a.get_file("{}".format(remotefile))
+            print("done")
+            
+            a.logout()
+            os.chdir(cwd)
+            
+            if progress_callback is not None:
+                progress_callback.emit(100)
+            
+        
+    def GetFile(self):
+        self.threadpool.start(self.worker)
+
+if __name__ == '__main__':
+
+    old = False
+    if old:
+        asyncCopy = AsyncCopyFromSSH()
+    else:
+        asyncCopy = AsyncCopyOverSSH()
+    
+    username = 'edo'
+    port = 22
+    host = 'vishighmem01.esc.rl.ac.uk'
+    private_key = "C:/Users/ofn77899/.ssh/id_rsa"
+    
+    asyncCopy.setRemoteConnectionSettings(username=username, 
+                                port=port, host=host, private_key=private_key)
+    if old:
+        asyncCopy.SetRemoteFileName(dirname='/home/edo', filename='head.mha')
+        asyncCopy.SetDestinationDir(os.path.abspath('.'))
+        asyncCopy.GetFile()
+    else:
+        import numpy as np
+        a = np.asarray([1,2,3])
+        np.save('test.npy',a)
+        asyncCopy.PutFile('test.npy', '/home/edo/pippo')
+        
+        while True:
+            tc = asyncCopy.threadpool.activeThreadCount()
+            if tc == 0:
+                break
+            # print (tc)
+            sleep(1)
+
+        os.rename('test.npy', 'test_orig.npy')
+        asyncCopy._worker = None
+        asyncCopy.GetFile('/home/edo/pippo/test.npy', os.path.abspath('.'))
+                
+        while True:
+            tc = asyncCopy.threadpool.activeThreadCount()
+            if tc == 0:
+                break
+            # print (tc)
+            sleep(1)
+
+        a = np.load('test_orig.npy')
+        b = np.load('test.npy')
+        np.testing.assert_array_equal(a,b)
+        os.remove('test.npy')
+        os.remove('test_orig.npy')
+        # put=False
+        # if put:
+        #     asyncCopy.GetFile('/home/edo/head.mha', os.path.abspath('.'))
+        # else:
+        #     asyncCopy.PutFile('head.mha', '/home/edo')
+
+    # need to keep the interpreter alive until the thread has finished
+    from time import sleep
+    while True:
+        tc = asyncCopy.threadpool.activeThreadCount()
+        if tc == 0:
+            break
+        # print (tc)
+        sleep(1)
