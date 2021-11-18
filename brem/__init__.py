bcbf7d0f6325f956b4f79a21c8d42acc7b4eb209
Edoardo Pasca <edo.paskino@gmail.com>
2021-11-18 16:02:56 +0000

    add Windows and POSIX in __init__.py

--- brem/__init__.py
+++ brem/__init__.py
@@ -26,6 +26,7 @@ __all__ = [basename(f)[:-3] for f in modules if isfile(f)
            and not f.endswith('__init__.py')]
 
 try:
+    from .brem import Windows, POSIX
     from .brem import BasicRemoteExecutionManager
     from .brem import RemoteRunControl
     from .brem import RemoteRunControlSignals
